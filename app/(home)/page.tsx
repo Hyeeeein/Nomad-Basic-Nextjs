ec24db981f6b936663797fd099ab011a1f4e83d1
hyein <khi_9866@naver.com>
2025-02-16 20:56:25 +0900

    feat: 3 완료

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,30 @@
+import Link from "next/link";
+
 // 특정 path 에서만 보여줘야 할 경우
 export const metadata = {
-  title: "home | next page",
+  title: "home | next page", // use client 에서는 사용x
 };
 
-export default function Tomato() {
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  // 클라이언트에서 보여주는 게 아니라 백엔드에서 실행되는 것이기 때문에 화면에는 보이지 않지만 탭에서 로딩이 돌고 있음
+  // 단점은 사용자는 모든 데이터를 가져오기 전에는 아무런 ui 를 볼 수 없다는 것(헤더, 메뉴바 등)
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(API_URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function Tomato() {
+  const movies = await getMovies();
   return (
     <div>
-      <h1>hello nextjs!</h1>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
     </div>
   );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,41 @@
-export default function MovieDetail({
+import { Suspense } from "react";
+import { API_URL } from "../../../(home)/page";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
+// async function getMovie(id: string) {
+//   console.log(`Fetching movies: ${Date.now()}`);
+//   await new Promise((resolve) => setTimeout(resolve, 5000));
+//   const response = await fetch(`${API_URL}/${id}`);
+//   return response.json();
+// }
+// async function getVideos(id: string) {
+//   console.log(`Fetching videos: ${Date.now()}`);
+//   await new Promise((resolve) => setTimeout(resolve, 5000));
+//   const response = await fetch(`${API_URL}/${id}/videos`);
+//   return response.json();
+// }
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <h1>Movie {id}</h1>;
+  // console.log("===========");
+  // console.log("start fetching");
+  // const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]); // 병렬, 배열 안의 모든 작업이 똑같은 시간에 시작한다
+  // console.log("end fetching");
+  // return <h1>{movie.title}</h1>;
+
+  return (
+    <div>
+      <h3>Movie detail page</h3>
+      {/* Suspense 로 인해 필요한 컴포넌트에만 로딩 화면 보여주기 가능 */}
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <h4>Videos</h4>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
